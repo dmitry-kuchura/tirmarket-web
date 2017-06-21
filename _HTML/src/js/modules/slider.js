5dd55b01ab3cea47cc1615e093c76885a2d74330
KholodarStas <kholodar.s.wezom@gmail.com>
2017-06-21 11:21:14 +0300

    item card

--- _HTML/src/js/modules/slider.js
+++ _HTML/src/js/modules/slider.js
@@ -7,7 +7,26 @@ function slider (context = 'body') {
         'promo': {
             infinite: false,
             speed: 300,
-            slidesToShow: 4
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },{
+                    breakpoint: 870,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },{
+                    breakpoint: 560,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
         }
     };
 

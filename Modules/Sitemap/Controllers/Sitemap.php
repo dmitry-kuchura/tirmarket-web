1d5eaae48a155749c7cd90fba9234a8e51260d47
Dmitry <kuchura.d@gmail.com>
2017-08-08 15:27:43 +0300

    some changes

--- Modules/Sitemap/Controllers/Sitemap.php
+++ Modules/Sitemap/Controllers/Sitemap.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Modules\Sitemap\Controllers;
 
 use Core\CommonI18n;
@@ -38,72 +39,72 @@ class Sitemap extends Base
         $this->_seo['title'] = $this->current->title;
         $this->_seo['keywords'] = $this->current->keywords;
         $this->_seo['description'] = $this->current->description;
-		
-		$map = Model::getRows(1,'sort','ASC');
-		$arr = [];
-		foreach ($map as $obj) {
-			$arr[$obj->parent_id][] = $obj;
-		}
-		
-		$links = [];
-		if (isset($map['content'])) {
-			$result = CommonI18n::factory('content')->getRows(1, 'sort', 'ASC');
-			$pages = [];
-			foreach ($result as $obj) {
-				$pages[$obj->parent_id][] = $obj;
-			}
-			$links['content'] = $pages;
-		}
-		
-		if (isset($map['news_list'])) {
-			$list = News::getRows(1, 'date', 'DESC');
-			$links['news_list'] = $list;
-		}
-		
-		if (isset($map['articles_list'])) {
-			$list = CommonI18n::factory('articles')->getRows(1, 'id', 'DESC');
-			$links['articles_list'] = $list;
-		}
-		
-		if (isset($map['blog_rubrics'])) {
-			$list = CommonI18n::factory('blog_rubrics')->getRows(1, 'sort', 'ASC');
-			$links['blog_rubrics'] = $list;
-		}
-		
-		if (isset($map['blog_list'])) {
-			$list = CommonI18n::factory('blog')->getRows(1, 'date', 'DESC');
-			$links['blog_list'] = $list;
-		}
-		
-		if (isset($map['gallery_list'])) {
-			$list = CommonI18n::factory('gallery')->getRows(1, 'sort', 'ASC');
-			$links['gallery_list'] = $list;
-		}
-		
-		if (isset($map['catalog_groups'])) {
-			$list =Groups::getRows(1, 'sort', 'ASC');
-			
-			$pages = [];
-			foreach ($list as $obj) {
-				$pages[$obj->parent_id][] = $obj;
-			}
-			$links['catalog_groups'] = $pages;
-		}
-		if (isset($map['catalog_items'])) {
-			$list = CommonI18n::factory('catalog')->getRows(1, 'sort', 'ASC');
-			$pages = [];
-			foreach ($list as $obj) {
-				$pages[$obj->parent_id][] = $obj;
-			}
-			$links['catalog_items'] = $pages;
-		}
-		
-		if (isset($map['brands_list'])) {
-			$list = Brands::getRows(1, 'brands.sort', 'ASC');
-			$links['brands_list'] = $list;
-		}
+
+        $map = Model::getRows(1, 'sort', 'ASC');
+        $arr = [];
+        foreach ($map as $obj) {
+            $arr[$obj->parent_id][] = $obj;
+        }
+
+        $links = [];
+        if (isset($map['content'])) {
+            $result = CommonI18n::factory('content')->getRows(1, 'sort', 'ASC');
+            $pages = [];
+            foreach ($result as $obj) {
+                $pages[$obj->parent_id][] = $obj;
+            }
+            $links['content'] = $pages;
+        }
+
+        if (isset($map['news_list'])) {
+            $list = News::getRows(1, 'date', 'DESC');
+            $links['news_list'] = $list;
+        }
+
+        if (isset($map['articles_list'])) {
+            $list = CommonI18n::factory('articles')->getRows(1, 'id', 'DESC');
+            $links['articles_list'] = $list;
+        }
+
+        if (isset($map['blog_rubrics'])) {
+            $list = CommonI18n::factory('blog_rubrics')->getRows(1, 'sort', 'ASC');
+            $links['blog_rubrics'] = $list;
+        }
+
+        if (isset($map['blog_list'])) {
+            $list = CommonI18n::factory('blog')->getRows(1, 'date', 'DESC');
+            $links['blog_list'] = $list;
+        }
+
+        if (isset($map['gallery_list'])) {
+            $list = CommonI18n::factory('gallery')->getRows(1, 'sort', 'ASC');
+            $links['gallery_list'] = $list;
+        }
+
+        if (isset($map['catalog_groups'])) {
+            $list = Groups::getRows(1, 'sort', 'ASC');
+
+            $pages = [];
+            foreach ($list as $obj) {
+                $pages[$obj->parent_id][] = $obj;
+            }
+            $links['catalog_groups'] = $pages;
+        }
+        if (isset($map['catalog_items'])) {
+            $list = CommonI18n::factory('catalog')->getRows(1, 'sort', 'ASC');
+            $pages = [];
+            foreach ($list as $obj) {
+                $pages[$obj->parent_id][] = $obj;
+            }
+            $links['catalog_items'] = $pages;
+        }
+
+        if (isset($map['brands_list'])) {
+            $list = Brands::getRows(1, 'brands.sort', 'ASC');
+            $links['brands_list'] = $list;
+        }
         // Render page
-        $this->_content = View::tpl(['result' => $arr, 'links'=>$links], 'Sitemap/Index');
+        $this->_content = View::tpl(['result' => $arr, 'links' => $links], 'Sitemap/Index');
     }
-	
+
 }
--- Views/Hidden/Auth.php
+++ Views/Hidden/Auth.php
@@ -1,23 +1,23 @@
 <?php use Core\HTML; ?>
 <div class="modal modal--sm">
     <div data-tab-content="sing-in" data-tab-ns="auth" class="is-active">
-        <div class="title title--md _mb-3">Войти в личный кабинет</div>
+        <div class="title title--md _mb-3"><?php echo __('Войти в личный кабинет'); ?></div>
 
         <form class="" data-form data-ajax="login">
-            <div class="form-label">Укажите свой Email <i>*</i></div>
+            <div class="form-label"><?php echo __('Укажите свой Email'); ?> <i>*</i></div>
             <div class="form-element _mb-2">
                 <input type="email" data-name="email" class="form-element__input" name="mail" data-rule-email="true"
                        required/>
             </div>
 
-            <div class="form-label">Пароль <i>*</i></div>
+            <div class="form-label"><?php echo __('Пароль'); ?> <i>*</i></div>
             <div class="form-element _mb-2">
-                <input type="password" data-name="password" class="form-element__input" name="pass"
+                <input type="password" data-name="password" data-msg="<?php ?>" class="form-element__input" name="pass"
                        data-rule-password="true" required/>
             </div>
 
             <div class="_mb-3">
-                <a href="#" data-tab-trigger="forgot" data-tab-ns="auth">Забыли пароль?</a>
+                <a href="#" data-tab-trigger="forgot" data-tab-ns="auth"><?php echo __('Забыли пароль'); ?>?</a>
             </div>
 
             <div class="form-element form-element--check _mb-3">
@@ -29,7 +29,7 @@
                                  xlink:href="<?php echo HTML::media('icons/icons.svg#check'); ?>"></use>
                         </svg>
                     </i>
-                    <span>Запомнить меня</span>
+                    <span><?php echo __('Запомнить меня'); ?></span>
                 </label>
             </div>
 
@@ -37,37 +37,37 @@
 
             <div class="_flex _justify-between _items-center">
                 <div class="_col-auto">
-                    <button class="button _pl-5 _pr-5" type="submit"><span>Войти</span></button>
+                    <button class="button _pl-5 _pr-5" type="submit"><span><?php echo __('Войти'); ?></span></button>
                 </div>
 
                 <div class="_col-auto">
-                    <a href="#" data-tab-trigger="sing-up" data-tab-ns="auth">Зарегистрироваться</a>
+                    <a href="#" data-tab-trigger="sing-up" data-tab-ns="auth"><?php echo __('Зарегистрироваться'); ?></a>
                 </div>
             </div>
         </form>
     </div>
 
     <div data-tab-content="sing-up" data-tab-ns="auth">
-        <div class="title title--md _mb-3">Зарегистрироваться</div>
+        <div class="title title--md _mb-3"><?php echo __('Зарегистрироваться'); ?></div>
 
         <form class="" data-form data-ajax="registration">
-            <div class="form-label">Укажите свой Email <i>*</i></div>
+            <div class="form-label"><?php echo __('Укажите свой Email'); ?> <i>*</i></div>
             <div class="form-element _mb-2">
                 <input type="email" class="form-element__input" name="mail" data-name="email" data-rule-email="true" required/>
             </div>
 
-            <div class="form-label">Пароль <i>*</i></div>
+            <div class="form-label"><?php echo __('Пароль'); ?> <i>*</i></div>
             <div class="form-element _mb-2">
                 <input type="password" class="form-element__input" name="pass" data-name="password" data-rule-password="true" required/>
             </div>
 
             <div class="_flex _justify-between _items-center _mb-3">
                 <div class="_col-auto">
-                    <a href="#" data-tab-trigger="sing-in" data-tab-ns="auth">Войти</a>
+                    <a href="#" data-tab-trigger="sing-in" data-tab-ns="auth"><?php echo __('Войти'); ?></a>
                 </div>
 
                 <div class="_col-auto">
-                    <a href="#" data-tab-trigger="forgot" data-tab-ns="auth">Забыли пароль?</a>
+                    <a href="#" data-tab-trigger="forgot" data-tab-ns="auth"><?php echo __('Забыли пароль'); ?>?</a>
                 </div>
             </div>
 
@@ -75,34 +75,34 @@
 
             <div class="_flex _justify-between _items-center">
                 <div class="_col-auto">
-                    <button class="button _pl-5 _pr-5" type="submit"><span>Зарегистрироваться</span></button>
+                    <button class="button _pl-5 _pr-5" type="submit"><span><?php echo __('Зарегистрироваться'); ?></span></button>
                 </div>
             </div>
         </form>
     </div>
 
     <div data-tab-content="forgot" data-tab-ns="auth">
-        <div class="title title--md _mb-3">Забыли пароль?</div>
+        <div class="title title--md _mb-3"><?php echo __('Забыли пароль'); ?>?</div>
 
         <form class="" data-form>
-            <div class="form-label">Укажите свой Email <i>*</i></div>
+            <div class="form-label"><?php echo __('Укажите свой Email'); ?> <i>*</i></div>
             <div class="form-element _mb-2">
                 <input type="email" class="form-element__input" name="mail" data-rule-email="true" required/>
             </div>
 
             <div class="_flex _justify-between _items-center _mb-3">
                 <div class="_col-auto">
-                    <a href="#" data-tab-trigger="sing-in" data-tab-ns="auth">Войти</a>
+                    <a href="#" data-tab-trigger="sing-in" data-tab-ns="auth"><?php echo __('Войти'); ?></a>
                 </div>
 
                 <div class="_col-auto">
-                    <a href="#" data-tab-trigger="sing-up" data-tab-ns="auth">Зарегистрироваться</a>
+                    <a href="#" data-tab-trigger="sing-up" data-tab-ns="auth"><?php echo __('Зарегистрироваться'); ?></a>
                 </div>
             </div>
 
             <div class="_flex _justify-between _items-center">
                 <div class="_col-auto">
-                    <button class="button _pl-5 _pr-5" type="submit"><span>Отправить</span></button>
+                    <button class="button _pl-5 _pr-5" type="submit"><span><?php echo __('Отправить'); ?></span></button>
                 </div>
             </div>
         </form>

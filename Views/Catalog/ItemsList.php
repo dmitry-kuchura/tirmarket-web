5fb1c70ff0ded3dbae1841f73aa5d296a5020f0b
Dmitry <kuchura.d@gmail.com>
2017-09-13 15:49:52 +0300

    some changes

--- Views/Catalog/ItemsList.php
+++ Views/Catalog/ItemsList.php
@@ -1,7 +1,11 @@
 <?php use Core\View; ?>
     <div class="_flex _grid-space-3">
-        <?php foreach ($result as $obj): ?>
-            <?php echo View::tpl(['obj' => $obj], 'Catalog/ItemTemplate'); ?>
-        <?php endforeach; ?>
+        <?php if (count($result)): ?>
+            <?php foreach ($result as $obj): ?>
+                <?php echo View::tpl(['obj' => $obj], 'Catalog/ItemTemplate'); ?>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p><?php echo __('Товары не найдены'); ?>!</p>
+        <?php endif; ?>
     </div>
 <?php echo $pager; ?>
--- Views/Widgets/Catalog/Sort.php
+++ Views/Widgets/Catalog/Sort.php
@@ -6,50 +6,54 @@ use Core\Route;
 
 $limit = Config::get('basic.limit');
 
-?>
-<div id="catalogSort" class="_col-auto _flex _items-center _grid-space-2 _lg-show"
-     data-uri="<?php echo str_replace('/page/' . Route::param('page'), '', Arr::get($_SERVER, 'REQUEST_URI')); ?>"
-     data-get="<?php echo Route::controller() == 'search' ? 'query=' . Arr::get($_GET, 'query') : ''; ?>">
-    <div class="_col-auto _flex _items-center _grid-space-2 _lg-show">
-        <div class="form-label"><?php echo __('Сортировать'); ?>:</div>
-        <div class="_col-auto">
-            <select name="sort" class="select">
-                <option value="cost"
-                        data-type="desc" <?php echo (Arr::get($_GET, 'sort') == 'cost' and Arr::get($_GET, 'type') == 'desc') ? 'selected' : ''; ?>>
-                    <?php echo __('От дорогих к дешевым'); ?>
-                </option>
-                <option value="cost"
-                        data-type="asc" <?php echo (Arr::get($_GET, 'sort') == 'cost' and Arr::get($_GET, 'type') == 'asc') ? 'selected' : ''; ?>>
-                    <?php echo __('От дешевых к дорогим'); ?>
-                </option>
-                <option value="created_at"
-                        data-type="desc" <?php echo (Arr::get($_GET, 'sort') == 'created_at' and Arr::get($_GET, 'type') == 'desc') ? 'selected' : ''; ?>>
-                    <?php echo __('От новых к старым'); ?>
-                </option>
-                <option value="created_at"
-                        data-type="asc" <?php echo (Arr::get($_GET, 'sort') == 'created_at' and Arr::get($_GET, 'type') == 'asc') ? 'selected' : ''; ?>>
-                    <?php echo __('От старых к новым'); ?>
-                </option>
-                <option value="name"
-                        data-type="asc" <?php echo (Arr::get($_GET, 'sort') == 'name' and Arr::get($_GET, 'type') == 'asc') ? 'selected' : ''; ?>>
-                    <?php echo __('По названию от А до Я'); ?>
-                </option>
-                <option value="name"
-                        data-type="desc" <?php echo (Arr::get($_GET, 'sort') == 'name' and Arr::get($_GET, 'type') == 'desc') ? 'selected' : ''; ?>>
-                    <?php echo __('По названию от Я до А'); ?>
-                </option>
-            </select>
-        </div>
-    </div>
+/* @var $hide boolean */
 
-    <div class="_col-auto _flex _items-center _grid-space-2 _lg-show">
-        <div class="form-label"><?php echo __('Показывать'); ?>:</div>
-        <div class="_col-auto">
-            <select name="per_page" class="select">
-                <?php for ($i = $limit; $i < $limit * 5; $i += $limit): ?>
-                    <option value="<?php echo $i; ?>" <?php echo Arr::get($_GET, 'per_page') == $i ? 'selected' : ''; ?>><?php echo $i; ?> <?php echo __('товаров'); ?></option>
-                <?php endfor; ?>
-            </select>
+?>
+<?php if ($hide == false): ?>
+    <div id="catalogSort" class="_col-auto _flex _items-center _grid-space-2 _lg-show"
+         data-uri="<?php echo str_replace('/page/' . Route::param('page'), '', Arr::get($_SERVER, 'REQUEST_URI')); ?>"
+         data-get="<?php echo Route::controller() == 'search' ? 'query=' . Arr::get($_GET, 'query') : ''; ?>">
+        <div class="_col-auto _flex _items-center _grid-space-2 _lg-show">
+            <div class="form-label"><?php echo __('Сортировать'); ?>:</div>
+            <div class="_col-auto">
+                <select name="sort" class="select">
+                    <option value="cost"
+                            data-type="desc" <?php echo (Arr::get($_GET, 'sort') == 'cost' and Arr::get($_GET, 'type') == 'desc') ? 'selected' : ''; ?>>
+                        <?php echo __('От дорогих к дешевым'); ?>
+                    </option>
+                    <option value="cost"
+                            data-type="asc" <?php echo (Arr::get($_GET, 'sort') == 'cost' and Arr::get($_GET, 'type') == 'asc') ? 'selected' : ''; ?>>
+                        <?php echo __('От дешевых к дорогим'); ?>
+                    </option>
+                    <option value="created_at"
+                            data-type="desc" <?php echo (Arr::get($_GET, 'sort') == 'created_at' and Arr::get($_GET, 'type') == 'desc') ? 'selected' : ''; ?>>
+                        <?php echo __('От новых к старым'); ?>
+                    </option>
+                    <option value="created_at"
+                            data-type="asc" <?php echo (Arr::get($_GET, 'sort') == 'created_at' and Arr::get($_GET, 'type') == 'asc') ? 'selected' : ''; ?>>
+                        <?php echo __('От старых к новым'); ?>
+                    </option>
+                    <option value="name"
+                            data-type="asc" <?php echo (Arr::get($_GET, 'sort') == 'name' and Arr::get($_GET, 'type') == 'asc') ? 'selected' : ''; ?>>
+                        <?php echo __('По названию от А до Я'); ?>
+                    </option>
+                    <option value="name"
+                            data-type="desc" <?php echo (Arr::get($_GET, 'sort') == 'name' and Arr::get($_GET, 'type') == 'desc') ? 'selected' : ''; ?>>
+                        <?php echo __('По названию от Я до А'); ?>
+                    </option>
+                </select>
+            </div>
+        </div>
+
+        <div class="_col-auto _flex _items-center _grid-space-2 _lg-show">
+            <div class="form-label"><?php echo __('Показывать'); ?>:</div>
+            <div class="_col-auto">
+                <select name="per_page" class="select">
+                    <?php for ($i = $limit; $i < $limit * 5; $i += $limit): ?>
+                        <option value="<?php echo $i; ?>" <?php echo Arr::get($_GET, 'per_page') == $i ? 'selected' : ''; ?>><?php echo $i; ?><?php echo __('товаров'); ?></option>
+                    <?php endfor; ?>
+                </select>
+            </div>
         </div>
     </div>
-</div>
+<?php endif; ?>

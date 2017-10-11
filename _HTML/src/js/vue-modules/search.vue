b1c9e5ced6ddfe4e245b4b188630c0ca662b0a73
Kholodar Stanislav <kholodar.s.wezom@gmail.com>
2017-10-11 17:56:53 +0300

    fix search

--- _HTML/src/js/vue-modules/search.vue
+++ _HTML/src/js/vue-modules/search.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="search-form">
-        <form @submit.prevent>
+        <form @submit.prevent="submit">
             <span class="form-element">
                 <input type="text"
                        class="form-element__input form-element__input--round"
@@ -9,7 +9,7 @@
                        v-model="search"
                        @keyup="change" />
 
-                <span class="search-form__icon">
+                <span class="search-form__icon" @click="submit">
                     <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/Media/icons/icons.svg#find"></use></svg>
                 </span>
             </span>
@@ -37,9 +37,16 @@
                 result: []
             }
         },
+
         props:['data-lang', 'data-placeholder'],
 
         methods: {
+
+            submit() {
+                if(this.search.length > 2)
+                    window.location.href = `${window.location.origin}/search?query=${this.search}`;
+            },
+
             change() {
                 let $this = this;
 
@@ -50,7 +57,7 @@
                     })
                         .then(function (response) {
                             $this.result.splice(0, $this.result.length);
-                            $this.result.push(...response.data.result);
+                            $this.result.push(...response.data.result.slice(0, 5));
                             $this.text = response.data.static;
                         })
                         .catch(function (error) {

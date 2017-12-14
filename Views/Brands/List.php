fed442204a2b81885c99cc4515128d4f20258312
Dmitry <kuchura.d@gmail.com>
2017-12-14 08:40:39 +0200

    Update List.php

--- Views/Brands/List.php
+++ Views/Brands/List.php
@@ -1,6 +1,10 @@
 <?php use Core\View; ?>
 <div class="_flex _grid-space-3">
-    <?php foreach ($result as $obj): ?>
-        <?php echo View::tpl(['obj' => $obj], 'Brands/ItemTemplateBrands'); ?>
-    <?php endforeach; ?>
+    <?php if (count($result)): ?>
+        <?php foreach ($result as $obj): ?>
+            <?php echo View::tpl(['obj' => $obj], 'Brands/ItemTemplateBrands'); ?>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p><?php echo __('Товары не найдены'); ?>!</p>
+    <?php endif; ?>
 </div>
